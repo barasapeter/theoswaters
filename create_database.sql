9d5235e4c5c6f7684707c81b21f080c714fee5f3
barasa <101450156+barasapeter@users.noreply.github.com>
2024-07-08 00:41:10 +0300

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS theoswaters;
 CREATE DATABASE theoswaters;
 USE theoswaters;
 
-
 CREATE TABLE IF NOT EXISTS merchants (
 	username VARCHAR(200) PRIMARY KEY,
     password VARCHAR(100)
