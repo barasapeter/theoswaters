6a48fc22b27fde8b9898e22a062e2393b3702092
barasa <101450156+barasapeter@users.noreply.github.com>
2025-02-18 07:36:08 +0300

    Update createdata.py

--- createdata.py
+++ createdata.py
@@ -1,5 +1,6 @@
 import database
 
+
 # alternatively run the `create_database.sql` query to achieve te same, cause basically that is what this script does
 with open('create_database.sql', 'r') as file:
     database.cursor.execute(''.join(file.readlines()))
